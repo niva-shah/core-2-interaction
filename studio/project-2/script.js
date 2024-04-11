736653a9f3ad3041fcda39d4acb753aaec7f2211
Niva <146101731+niva-shah@users.noreply.github.com>
2024-04-11 03:55:09 -0400

    Add files via upload

---
+++ studio/project-2/script.js
@@ -0,0 +1,255 @@
+let testJSON = [
+  {
+    "Object": "magazine Pole Position",
+    "Where i got it": "New York",
+    "Discription": "A magazine created by me about Formula 1",
+    "x": 1106,
+    "y": 796,
+    "Links": "https://nivashah.myportfolio.com/pole-position"
+  },
+  {
+    "Object": "MacBook",
+    "Where i got it": "New York",
+    "Discription": "A silver macbook pro ",
+    "x": 1031,
+    "y": 613,
+    "Links": "https://www.apple.com/macbook-pro/"
+  },
+  {
+    "Object": "rings",
+    "Where i got it": "India",
+    "Discription": "Chunky Gold Rings",
+    "x": 1226,
+    "y": 735,
+    "Links": "https://www.freepeople.com/shop/essence-ring/?category=SEARCHRESULTS&color=070&searchparams=color%3Dgreen%26q%3Drings&_gl=1%2aqfa940%2a_up%2aMQ..&gclid=Cj0KCQjw8J6wBhDXARIsAPo7QA_skDSAkwui7zxejOru2YNPEPrn5UX-YRx-glDkcQUATDhm5t0yR-saAo18EALw_wcB&gclsrc=aw.ds&type=REGULAR&quantity=1"
+  },
+  {
+    "Object": "Pentax film camera",
+    "Where i got it": "India",
+    "Discription": "Dark grey Pentax IQZoom140",
+    "x": 1035,
+    "y": 699,
+    "Links": "https://filmsupply.club/products/pentax-iqzoom-140-point-and-shoot-camera"
+  },
+  {
+    "Object": "jaquemuse bag",
+    "Where i got it": "India",
+    "Discription": "bottle green bag with gold embosing",
+    "x": 970,
+    "y": 751,
+    "Links": "https://www.jacquemus.com/en_us/le-chiquito-moyen/213BA002-3163-590.html"
+  },
+  {
+    "Object": "Vanity fair magazine",
+    "Where i got it": "New York",
+    "Discription": "vanity fair magazine - september 2022 issue lewis hamilton beyond finish line ",
+    "x": 870,
+    "y": 554,
+    "Links": "https://www.vanityfair.com/style/2022/08/cover-story-lewis-hamilton-never-quits"
+  },
+  {
+    "Object": "Grada Bangle",
+    "Where i got it": "San Juan",
+    "Discription": "Gold bangle with green gems ",
+    "x": 721,
+    "y": 583,
+    "Links": "https://shopvalija.com/collections/jewelry-bracelets/products/grada-bangle?variant=40544512999511"
+  },
+  {
+    "Object": "Eucalyptus Spearmint candle",
+    "Where i got it": "New York",
+    "Discription": "3-wick Eucalyptus Spearmint scent candle ",
+    "x": 483,
+    "y": 504,
+    "Links": "https://www.bathandbodyworks.com/p/eucalyptus-spearmint-3-wick-candle-026617002.html#q=eucalyptus&start=2"
+  },
+  {
+    "Object": "Dupatta",
+    "Where i got it": "India",
+    "Discription": "colorful indian patola print dupatta ",
+    "x": 662,
+    "y": 500,
+    "Links": "https://en.wikipedia.org/wiki/Patola_sari"
+  },
+  {
+    "Object": "Elephant",
+    "Where i got it": "Bali",
+    "Discription": "Grey plush elephant",
+    "x": 1169,
+    "y": 486,
+    "Links": "https://www.balisafarimarinepark.com/"
+  },
+  {
+    "Object": "The MET map",
+    "Where i got it": "New York",
+    "Discription": "Red folded brochure Map",
+    "x": 1434,
+    "y": 249,
+    "Links": "https://maps.metmuseum.org/?floor=1#17.78/40.779456/-73.963523/-61"
+  },
+  {
+    "Object": "Ferrari LEGO car",
+    "Where i got it": "Abu Dhabi",
+    "Discription": "Red 970 Ferrari 512 M – 76906 LEGO Car model",
+    "x": 1239,
+    "y": 147,
+    "Links": "https://www.lego.com/en-us/product/1970-ferrari-512-m-76906?gclid=Cj0KCQjw8J6wBhDXARIsAPo7QA-tTQZzJXGldtAuF_OXUh9124u__Ys9qQ4xqU2kmu_kwlSTg9tMSkUaAuasEALw_wcB&ef_id=Cj0KCQjw8J6wBhDXARIsAPo7QA-tTQZzJXGldtAuF_OXUh9124u__Ys9qQ4xqU2kmu_kwlSTg9tMSkUaAuasEALw_wcB%3AG%3As&s_kwcid=AL%21790%213%21%21%21%21x%21%21%2119932189580%21&cmp=KAC-INI-GOOGUS-GO-US_GL-EN-RE-SP-BUY-CREATE-MB_ALWAYS_ON-SHOP-BP-PMAX-ALL-CIDNA00000-PMAX-LOW_PRIORITY&gad_source=1"
+  },
+  {
+    "Object": "Sona image",
+    "Where i got it": "New York",
+    "Discription": "A black and white photograph print of my friend sona taken by me",
+    "x": 1101,
+    "y": 167,
+    "Links": "https://nivashah.myportfolio.com/165701b2dcf172"
+  },
+  {
+    "Object": "Film rolls",
+    "Where i got it": "New York",
+    "Discription": "Kentmere Pan 100 Black and White Negative Film (35mm Roll Film, 36 Exposures)",
+    "x": 596,
+    "y": 144,
+    "Links": "https://www.bhphotovideo.com/c/product/601035-REG/Kentmere_6010465_Kentmere_35mm_Black_and.html/?ap=y&ap=y&smp=y&smp=y&lsft=BI%3A514&gad_source=1&gclid=Cj0KCQjw8J6wBhDXARIsAPo7QA9cqwTAvnI3P9He4dIn0_1eFsJfRa9Irfjcunjtl2HajYQXcoesMSUaAhTFEALw_wcB"
+  },
+  {
+    "Object": "Ferrari book",
+    "Where i got it": "New York",
+    "Discription": "Dream in Red - Ferrari by Maggi & Maggi: A photographic journey through the finest cars ever made",
+    "x": 456,
+    "y": 429,
+    "Links": "https://www.thriftbooks.com/w/dream-in-red---ferrari-by-maggi--maggi-a-photographic-journey-through-every-car/37612267/item/54499597/?utm_source=google&utm_medium=cpc&utm_campaign=pmax_new_books&utm_adgroup=&utm_term=&utm_content=&gad_source=1&gclid=Cj0KCQjw8J6wBhDXARIsAPo7QA9dp0IDfJds4CoCuciUC3dNiQF5_bIMBDAR_WTBZjKJjHOW6MgRr7kaArIdEALw_wcB#idiq=54499597&edition=65586483"
+  },
+  {
+    "Object": "miumiu heels",
+    "Where i got it": "New York",
+    "Discription": "Cherry red miumiu heels ",
+    "x": 228,
+    "y": 325,
+    "Links": "https://www.miumiu.com/us/en/p/patent-leather-slingbacks-with-buckles/5I013E_069_F0D27_F_M105?utm_campaign=GoogleShopping_US&utm_medium=CPC&utm_source=Google&utm_content=PMax&s_kwcid=AL!8549!3!!!!x!!&gad_source=1&gclid=Cj0KCQjw8J6wBhDXARIsAPo7QA9wYyOFf967VmK0Zs8LgUs_pRR18sK_tAv0bC7HrKU6xIkzr8StacsaApUSEALw_wcB&gclsrc=aw.ds"
+  },
+  {
+    "Object": "Cowboy boots",
+    "Where i got it": "India",
+    "Discription": "Knee-high beige brown H&M cowboy boots",
+    "x": 381,
+    "y": 728,
+    "Links": "https://www2.hm.com/en_us/productpage.1181120001.html"
+  },
+  {
+    "Object": "Infusion de Mandarine Eau de Parfum",
+    "Where i got it": "Dubai",
+    "Discription": "Manderin scented prada perfume ",
+    "x": 723,
+    "y": 540,
+    "Links": "https://www.prada-beauty.com/fragrance/les-infusions/infusion-de-mandarine-eau-de-parfum/MPL01381.html?gad_source=1&gclid=Cj0KCQjw8J6wBhDXARIsAPo7QA_QQ6Fh6Xu7Wy6aLn_rvcDGCI7zUbUejLa03j38wJmotejvEFgHpmcaAosUEALw_wcB&gclsrc=aw.ds"
+  },
+  {
+    "Object": "AirPods ",
+    "Where i got it": "Chicago",
+    "Discription": "Apple AirPods Pro (2nd generation) in a lavender case ",
+    "x": 644,
+    "y": 744,
+    "Links": "https://www.apple.com/airpods-pro/"
+  },
+  {
+    "Object": "Sketch book",
+    "Where i got it": "New York",
+    "Discription": "Black Moleskine Hardcover sketchbook",
+    "x": 687,
+    "y": 787,
+    "Links": "https://www.dickblick.com/products/moleskine-classic-hardcover-notebooks/?fromSearch=%2Fsearch%2F%3Fsearchword%3Dblack%20sketchbook"
+  },
+  {
+    "Object": "Bose speaker",
+    "Where i got it": "San Francisco ",
+    "Discription": "Dark Grey Bose SoundLink Mini II Special Edition ",
+    "x": 404,
+    "y": 549,
+    "Links": "https://www.bose.com/p/speakers/bose-soundlink-mini-ii-special-edition/SLMINIISE-SPEAKERWIRELESS-TRPBLK-WW.html"
+  },
+  {
+    "Object": "Star face",
+    "Where i got it": "New York",
+    "Discription": "yello hydro-stars in a yellow starface box",
+    "x": 730,
+    "y": 676,
+    "Links": "https://starface.world/products/hydro-stars-big-yellow?currency=USD&g_acctid=645-271-4635&g_adgroupid=&g_adid=&g_adtype=none&g_campaign=DSGM%7CPerformanceMax%7CAll%7CUS&g_campaignid=17337703839&g_keyword=&g_keywordid=&g_network=x&gad_source=1&gclid=Cj0KCQjw8J6wBhDXARIsAPo7QA9T9j-FF7cdxlp7LYAMWSRVTtlB3_UYt0iaqJ6DgaTouRuRQ99-lmwaAvSyEALw_wcB&utm_campaign=sag_organic&utm_content=sag_organic&utm_medium=product_sync&utm_source=google&variant=31344717234247"
+  },
+  {
+    "Object": "Summer Fridays",
+    "Where i got it": "New York",
+    "Discription": "Shiney blue tube of the Jet Lag Mask (Mini Travel Size)",
+    "x": 784,
+    "y": 658,
+    "Links": "https://summerfridays.com/products/mini-jet-lag-mask?variant=40532146094157&currency=USD&utm_medium=product_sync&utm_source=google&utm_content=sag_organic&utm_campaign=sag_organic&tw_source=google&tw_adid=&tw_campaign=16405454081&gad_source=1&gclid=Cj0KCQjw8J6wBhDXARIsAPo7QA8Wsko4z2AEp_dYRL2GHgjrejdL8TUjFCynlLCY3UJ6DQWDKid5Yy4aArvoEALw_wcB"
+  },
+  {
+    "Object": "retro aviator sunglasse",
+    "Where i got it": "New York",
+    "Discription": "Blue tinted lens vintage retro aviator sunglasse with a leopard print body",
+    "x": 959,
+    "y": 606,
+    "Links": "https://www.amazon.com/dp/B09NLY99SF?psc=1&ref=ppx_yo2ov_dt_b_product_details"
+  },
+  {
+    "Object": "Enchanté", 
+    "Where i got it": "New York",
+    "Discription": "Beaded bracelet",
+    "x": 1176,
+    "y": 645,
+    "Links": "https://enchante.co/"
+  },
+  {
+    "Object": "Sabyasachi Pouch", 
+    "Where i got it": "New York",
+    "Discription": "sabyasachi beige printed velvet jewelry pouch",
+    "x": 1228,
+    "y": 688,
+    "Links": "https://sabyasachi.com/"
+  }
+];
+let materialList = document.getElementById("container");
+for (let i = 0; i <testJSON.length; i++) {
+  let newDIV = document.createElement("div");
+  newDIV.classList.add("item_link");
+
+  newDIV.addEventListener("click", function () {
+    window.open(testJSON[i].Links, "_blank");
+  }); 
+
+  newDIV.style.left = testJSON[i].x + 'px'; 
+  newDIV.style.top = testJSON[i].y + 'px';
+  newDIV.addEventListener("mouseover", mouseOverFunction);
+
+  function mouseOverFunction() {
+  console.log("TEST");
+ }
+
+ function clickEvent(a) {
+  console.log("click");
+ }
+  let newA = document.createElement("a");
+  newA.href = testJSON[i].Links;
+  // console.log(testJSON[i].Links);
+  // newA.innerText="*";
+
+  newDIV.appendChild(newA);
+  materialList.appendChild(newDIV);
+}
+
+
+//Notes on coding with Andrew
+// When converting to JSON: no spaces in your headers (which your JSON values)
+// Any numbers need quotations removed from them
+ // let materialList = document.getElementById("container");
+  //for (let i = 0; i <testJSON.length; i++) {
+   // let newSPAN = document.createElement("a");
+    //newSPAN.setAttribute('href', 'testJSON[i].Links');
+   // newSPAN.style.left = testJSON[i].x + 'px'; // setting the x coordinate
+    //newSPAN.style.top = testJSON[i].y + 'px'; // setting the y coordinate
+    //newSPAN.innerText=testJSON[i].item;
+    //materialList.appendChild(newSPAN);
+ // }
+// When converting to JSON: no spaces in your headers (which your JSON values)
+// Any numbers need quotations removed from them
+
