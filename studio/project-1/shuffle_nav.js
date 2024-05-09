7859de7aea5a7d6af2ff54c993819f85169ed2c7
Niva <146101731+niva-shah@users.noreply.github.com>
2024-05-09 07:20:45 -0400

    Add files via upload

---
+++ studio/project-1/shuffle_nav.js
@@ -0,0 +1,13 @@
+<div class="content">
+<nav class="quote-navigation">
+    <a href="../project-1/page 1/quote1.html">1</a>
+    <a href="../project-1/page 2/quote2.html">2</a>
+    <a href="../project-1/page 3/quote3.html">3</a>
+    <a href="../project-1/page 4/quote4.html">4</a>
+    <a href="../project-1/page 5/quote5.html">5</a>
+    <a href="../project-1/page 6/quote6.html">6</a>
+    <a href="../project-1/page 7/quote7.html">7</a>
+    <a href="../project-1/page 8/quote8.html">8</a>
+</nav>
+
+<script src="shuffle_nav.js"></script>
